d89549ae38dd7052bd00ad6e0357447c7ef369f8
Lachlan Deakin <ljdgit@gmail.com>
2024-03-18 17:18:22 +1100

    Generalise Findzarrs.cmake

--- examples/cmake_project/Findzarrs.cmake
+++ examples/cmake_project/Findzarrs.cmake
@@ -1,6 +1,9 @@
 include(FetchContent)
 
-option(ZARRS_USE_EXPERIMENTAL_CODECS "enable experimental zarrs codecs" ON)
+# Example Usage
+# find_package(zarrs 0.5.0 REQUIRED COMPONENTS zarrs/bz2)
+# Components are zarrs optional codecs, such as:
+#   zarrs/bitround zarrs/zfp zarrs/bz2 zarrs/pcodec
 
 # Corrosion: integrate rust into a cmake project
 FetchContent_Declare(Corrosion
@@ -11,20 +14,13 @@ FetchContent_MakeAvailable(Corrosion)
 
 # zarrs_ffi: C/C++ bindings to zarrs
 FetchContent_Declare(zarrs_ffi_source
-    SOURCE_DIR "${CMAKE_CURRENT_SOURCE_DIR}/../.."
-    # GIT_REPOSITORY https://github.com/LDeakin/zarrs_ffi.git
-    # GIT_TAG v0.5.0
+    GIT_REPOSITORY https://github.com/LDeakin/zarrs_ffi.git
+    GIT_TAG v${zarrs_FIND_VERSION}
 )
 FetchContent_Populate(zarrs_ffi_source)
 
 # Build zarrs_ffi, creates a zarrs_ffi target aliased to zarrs::zarrs
-if(ZARRS_USE_EXPERIMENTAL_CODECS)
-  set(ZARRS_FEATURES zarrs/bitround zarrs/zfp zarrs/bz2 zarrs/pcodec)
-  message(STATUS "Enabling experimental zarrs codecs: ${ZARRS_FEATURES}")
-else()
-  set(ZARRS_FEATURES)
-endif()
-corrosion_import_crate(MANIFEST_PATH ${zarrs_ffi_source_SOURCE_DIR}/Cargo.toml FEATURES ${ZARRS_FEATURES})
+corrosion_import_crate(MANIFEST_PATH ${zarrs_ffi_source_SOURCE_DIR}/Cargo.toml FEATURES ${zarrs_FIND_COMPONENTS})
 # corrosion_experimental_cbindgen(TARGET zarrs_ffi HEADER_NAME "zarrs.h") # not working
 target_include_directories(zarrs_ffi INTERFACE ${zarrs_ffi_source_SOURCE_DIR}) # add zarrs.h to include directories
 add_library(zarrs::zarrs ALIAS zarrs_ffi)
